b9e1622cea50baed5800ad2ecd23706b02f83797
luzilong <luzilong2005@foxmail.com>
2025-11-20 17:30:15 +0800

    更新配置

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
     plugins: [vue(), tailwindcss()],
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        "@": path.resolve(__dirname, "src"),
       },
     },
     server: {
